1e2d794880ce4fc3218c1174875869319aec2608
Prakash Surya <prakash.surya@delphix.com>
2018-06-18 13:03:53 -0700

    WIP: Add inital scripts to build "seed repository"
    This change adds some initial logic to build the "seed repository" based
    on the dependencies of the "delphix-foundation" package. This is simply
    meant to serve as a proof-of-concept, rather than a finished work. After
    building the "delphix-foundation" metapackage, that package's recursive
    dependencies are determined, and then each dependent package is pulled
    down and inserted into a local repository using Aptly.
    
    The intention is for this local Aptly repository to then be used by
    live-build, such that our appliance is generated from this local
    repository rather than from Ubuntu's repository. Currently though, this
    local repository is not consumed by anything.
    
    Further, this concept needs to also be applied to our other delphix
    packages (e.g. virtualization, masking), so that we populate this seed
    repository with all of the recursive dependencies of those packages.
    Additionally, we need to determine how to populate this seed repository
    with the various packages currently installed via the appliance-build
    Ansible roles (e.g. compilers, tools, etc.).
    
    Some initial testing/examples are below:
    
        $ time ./scripts/docker-run.sh make seed-repository
        ...
        real    5m42.859s
        user    0m0.074s
        sys     0m0.093s
    
        $ ls -lh artifacts/
        total 442M
        -rw-r--r-- 1 root root 442M Jun 20 23:58 seed-repository.tar.gz
    
        $ tar -tf artifacts/seed-repository.tar.gz | egrep '.deb$' | head
        ./pool/5e/fc/c49e28f1bf51b04248f32a38a88d_tshark_2.4.5-1_amd64.deb
        ./pool/5e/e2/ac50a7e374a8312fb68c64da27da_libxrandr2_2:1.5.1-1_amd64.deb
        ./pool/5e/cd/8162a82d8c601d8951be2dcd1934_python3-jwt_1.5.3+ds1-1_all.deb
        ./pool/d4/51/eb00b2d0e2940e26028ea9f2fb1b_adduser_3.116ubuntu1_all.deb
        ./pool/8f/ed/f4bd27fc3f5d7b2fe3f1412e9221_gzip_1.6-5ubuntu1_amd64.deb
        ./pool/ff/c4/8ddd362288e45525f12a9a84eae5_libfreetype6_2.8.1-2ubuntu2_amd64.deb
        ./pool/ff/16/4729742f8e8cfc1024d6b62d9d7d_libpython3.6-stdlib_3.6.5-3_amd64.deb
        ./pool/ff/96/a9900df192e718262fc12ccb0822_python3-markupsafe_1.0-1build1_amd64.deb
        ./pool/8c/06/8498210438759bc0daafbb1cf74d_libgif7_5.1.4-2_amd64.deb
        ./pool/8c/4a/d3f19ad9b5d34975a7973d195292_libcap2-bin_1:2.25-1.2_amd64.deb
    
        $ tar -tf artifacts/seed-repository.tar.gz | egrep '.deb$' | wc -l
        1130

---
+++ scripts/build-seed-repository.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+#
+# Copyright 2018 Delphix
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+function list_depends_of_deb
+{
+	dpkg -I "$TOP/delphix-foundation_1.0.0_amd64.deb" |
+		grep Depends |
+		cut -d ':' -f 2 |
+		tr -d '[:blank:]' |
+		tr ',' '\n'
+}
+
+function list_essential_pkgs
+{
+	aptitude search \
+		'?essential' \
+		'?priority(required)' \
+		--display-format '%p'
+}
+
+function germinate_seed_pkgs
+{
+	local TMP_DIRECTORY=$(mktemp -d -p . tmp.germinate.XXXXXXXXXX)
+
+	pushd "$TMP_DIRECTORY" &>/dev/null
+
+	mkdir -p ubuntu.bionic
+	touch ubuntu.bionic/blacklist
+	touch ubuntu.bionic/seeds
+	echo "seeds:" > ubuntu.bionic/STRUCTURE
+
+	while read pkg; do
+		echo " * $pkg" >> ubuntu.bionic/seeds
+	done
+
+	germinate -S . \
+		-c main,restricted,universe,multiverse \
+		-m http://archive.ubuntu.com/ubuntu \
+		-m http://security.ubuntu.com/ubuntu \
+		1>&2
+
+	tail -n +3 seeds | head -n -2 | cut -d '|' -f 1
+
+	popd &>/dev/null
+	rm -rf "$TMP_DIRECTORY"
+}
+
+function download_pkgs
+{
+	xargs apt-get \
+		-o Dir::Cache::Archives="$1" \
+		--download-only \
+		--assume-yes \
+		--reinstall \
+		install
+}
+
+TOP=$(git rev-parse --show-toplevel 2>/dev/null)
+
+if [[ -z "$TOP" ]]; then
+	echo "Must be run inside the git repsitory."
+	exit 1
+fi
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+TMP_DIRECTORY=$(mktemp -d -p . tmp.deps.XXXXXXXXXX)
+
+#
+# Before we attempt to fetch any of the packages, we want to ensure our
+# apt cache is up-to-date; otherwise we could fetch old packages, or
+# simply not find any packages to fetch because the cache is empty.
+#
+apt-get update
+
+#
+# First, we want to download all recursive dependencies for our
+# foundation package.
+#
+list_depends_of_deb "$TOP/delphix-foundation_1.0.0_amd64.deb" |
+	germinate_seed_pkgs |
+	download_pkgs "$TMP_DIRECTORY"
+
+#
+# Next, we need to ensure we download and include all essential
+# packages, since these are necessary for a working system, but are not
+# necessarily listed as dependencies of our foundation; e.g. coreutils.
+#
+list_essential_pkgs | download_pkgs "$TMP_DIRECTORY"
+
+#
+# Lastly, we need to include the "dctrl-tools" package, since this is a
+# requirement of live-build, and is necessary to use the seed-repository
+# as the bootstrap-mirror input to live-build
+#
+echo "dctrl-tools" | download_pkgs "$TMP_DIRECTORY"
+
+#
+# After downloading the packages, the package filenames any have the
+# sequence of characters "%3a" embedded in them. These characters cause
+# problems when the files are exported over HTTP via the Aptly served
+# repository. Thus, we convert this sequence back to the original ":"
+# character (which is what the sequence represents) as a workaround, so
+# the files can be properly served by Aptly.
+#
+rename 's/\%3a/:/g' "$TMP_DIRECTORY"/*.deb
+
+#
+# And now, we can create the Aptly repository using all of the .deb
+# packages that were download previously, plus our metapackages.
+#
+
+rm -rf ~/.aptly
+aptly repo create -distribution=bionic -component=delphix seed-repository
+
+aptly repo add seed-repository "$TMP_DIRECTORY"
+aptly repo add seed-repository "$TOP/delphix-foundation_1.0.0_amd64.deb"
+
+aptly snapshot create seed-repository-snapshot from repo seed-repository
+aptly publish snapshot -skip-signing seed-repository-snapshot
+
+tar -czf "$TOP/artifacts/seed-repository.tar.gz" -C ~/.aptly .
+
+rm -rf "$TMP_DIRECTORY"
